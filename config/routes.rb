318410149ad79d45a08d3131f665c2e35c21d06e
Jason Humphries <humphriesj@gmail.com>
2013-12-10 20:57:15 -0500

    starting back

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 RtdRailsApp::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  
+  # home routes
+  get "home/index"
+  get "home/about"
+  get "home/events"
+  get "home/registration"
+  get "home/social"
+  get "home/press"
+  get "home/contact"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :events
+  resources :users
+
+  # root route
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
