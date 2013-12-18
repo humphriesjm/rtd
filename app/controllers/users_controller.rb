ac87146e70373f482c149dad33174440aac3daea
Jason Humphries <humphriesj@gmail.com>
2013-12-17 21:23:26 -0500

    improves user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(event_params)
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        binding.pry
-        ContactMailer.welcome_email(@user).deliver
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        # binding.pry
+        # ContactMailer.welcome_email(@user).deliver
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
+        binding.pry
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -31,7 +32,7 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
-      if @user.update(event_params)
+      if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
@@ -44,19 +45,19 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to events_url }
+      format.html { redirect_to users_url }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @user = User.find(params[:id])
-    end
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:name, :address, :email, :phone, :org, :birthdate, :weight, :height, :sex)
+    def user_params
+      params.require(:user).permit(:name, :address, :email, :phone, :org, :birthdate, :weight, :height, :sex)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :reservations
 	has_many :events, through: :reservations
+
+	validates :name, presence: true
+	validates :org, presence: true
+	validates :email, presence: true
 end
