77746b7d696dc5854efc5fb7293f3f67b745dee4
Vojtech Rinik <vojto@rinik.net>
2014-01-28 16:54:32 -0500

    Adding mocha

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
 gem "twitter-bootstrap-rails"
 gem "rspec-rails"
+gem "mocha"
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- spec/helpers/authentication_helper_spec.rb
+++ spec/helpers/authentication_helper_spec.rb
@@ -23,5 +23,15 @@ describe AuthenticationHelper do
       session[:user_id] = nil
       helper.current_user.should == nil
     end
+
+    it "only looks up user once" do
+      User.expects(:find).with(1).once
+
+      session[:user_id] = 1
+
+      helper.current_user
+      helper.current_user
+      helper.current_user
+    end
   end
 end
