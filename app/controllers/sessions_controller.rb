f620cab2b52d929496756c1373be4117cdac0c18
Jason Humphries <humphriesj@gmail.com>
2014-01-28 15:36:52 -0500

    adds session #create test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,9 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+    	session[:user_id] = user.id
+    end
     render nothing: true
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,22 +1,38 @@
 require "spec_helper"
 
 describe SessionsController do
+  let(:user_params) do
+    {
+      name: 'foo',
+      email: 'foo@bar.com',
+      org: 'foo inc',
+      password: 'foo',
+      password_confirmation: 'foo'
+    }
+  end
+  let(:session_params) do
+    {
+      email: 'foo@bar.com',
+      password: 'foo'
+    }
+  end
+
   describe "#create" do
     it "stores user id in the session" do
-      user = User.create(
-        name: 'foo',
-        email: 'foo@bar.com',
-        org: 'foo inc',
-        password: 'foo',
-        password_confirmation: 'foo'
-      )
+      user = User.create(user_params)
 
-      post :create, session: {
-        email: 'foo@bar.com',
-        password: 'foo'
-      }
+      post :create, session: session_params
       
       session[:user_id].should == user.id
     end
+
+    it "redirects the home page on successful login" do
+      user = User.create(user_params)
+
+      post :create, session: session_params
+
+      response.should redirect_to(root_path)
+    end
   end
+
 end
