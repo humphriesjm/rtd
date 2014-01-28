6c446e7e25c24d76c6ad3c14de3c994291e7cb78
Vojtech Rinik <vojto@rinik.net>
2014-01-28 15:53:38 -0500

    Adding DatabaseCleaner

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -20,15 +20,13 @@ describe SessionsController do
   describe "#create" do
     it "stores user id in the session" do
       user = User.create(user_params)
-
       post :create, session: session_params
-      
+    
       session[:user_id].should == user.id
     end
 
     it "redirects the home page on successful login" do
       user = User.create(user_params)
-
       post :create, session: session_params
 
       response.should redirect_to(root_path)
