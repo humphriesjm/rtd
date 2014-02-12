89a3da7ae5a031f4bb610e14c0e8d4df0337458d
Jason Humphries <humphriesj@gmail.com>
2014-02-12 16:25:13 -0500

    changes from sqlite3 to mysql2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,49 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "RTD"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git
+set :repo_url, "git@github.com:humphriesjm/rtd.git"
+set :deploy_to, "/opt/rtd_rails_app/"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :ssh_options, { forward_agent: true }
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :log_level, :debug
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# ask :branch, "master"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :format, :pretty
+set :pty, true
+
+# SSHKit.config.command_map[:rake]  = "bundle exec rake" #8
+# SSHKit.config.command_map[:rails] = "bundle exec rails"
+
+set :keep_releases, 20
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
